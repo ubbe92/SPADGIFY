e87ba651f73e0d373c66c966ed56c73fb39ca6e7
Anton Dacklin <ens19ann@cs.umu.se>
2024-10-02 17:02:26 +0200

    backup

--- java/spadgify/src/main/java/cs/umu/se/util/MediaUtil.java
+++ java/spadgify/src/main/java/cs/umu/se/util/MediaUtil.java
@@ -22,7 +22,7 @@ public class MediaUtil {
         int duration = chordMediaInfo.getDuration();
         String genre = chordMediaInfo.getGenre();
         long size = chordMediaInfo.getSize();
-        String identifierString = song + ":" + artist + ":" + album;
+        String identifierString = song + "-" + artist + "-" + album;
         int hash = Hash.getNodeIdentifierFromString(identifierString, m);
 
         return new MediaInfo(artist, song, album, duration, genre, size, hash);
