a19140f690d175b4c11f9d5f3399aef2e143dcd4
Anton Dacklin <ens19ann@cs.umu.se>
2024-10-09 12:15:36 +0200

    fixed script

--- deploy/copyChordJarToTestFolders.sh
+++ deploy/copyChordJarToTestFolders.sh
@@ -1,24 +1,14 @@
 #!/usr/bin/env bash
 
-cd ./../testChord/8185
-rm spadgify-1.0-SNAPSHOT.jar
-
-cd ./../8187
-rm spadgify-1.0-SNAPSHOT.jar
-
-cd ./../8188
-rm spadgify-1.0-SNAPSHOT.jar
-
-cd ./../8193
-rm spadgify-1.0-SNAPSHOT.jar
-
-cd ./../8206
-rm spadgify-1.0-SNAPSHOT.jar
+cd ./../
+rm -rf testChord
+mkdir testChord
+cd testChord
+mkdir 8185 8187 8188 8193 8206
 
 echo "Removed old jar in test folders!"
 
-#cd ./../java/spadgify/target
-cd ./../../java/spadgify/target
+cd ./../java/spadgify/target
 cp spadgify-1.0-SNAPSHOT.jar ../../../testChord/8185
 cp spadgify-1.0-SNAPSHOT.jar ../../../testChord/8187
 cp spadgify-1.0-SNAPSHOT.jar ../../../testChord/8188
