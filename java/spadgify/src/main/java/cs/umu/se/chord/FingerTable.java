d067ceda1a05af0e6fbd421b0a372184ed7a7731
Sinthujan <sinthujan.ponnampalam@outlook.com>
2024-10-09 14:39:54 +0200

    FingerTable refactor

--- java/spadgify/src/main/java/cs/umu/se/chord/FingerTable.java
+++ java/spadgify/src/main/java/cs/umu/se/chord/FingerTable.java
@@ -1,10 +1,13 @@
 package cs.umu.se.chord;
 
+/**
+ * Class to handle finger table for a node
+ */
 public class FingerTable {
-    private int m;
+    private final int m;
     private int myIdentifier;
-    private FingerTableEntry[] table;
-    private Node thisNode;
+    private final FingerTableEntry[] table;
+    private final Node thisNode;
 
     public FingerTable(Node thisNode) {
         this.m = thisNode.getM();
@@ -13,17 +16,29 @@ public class FingerTable {
         this.table = new FingerTableEntry[m];
 
         // Calculate the start and intervals and put this node as the successor node for each entry
-        fillTableWithEntries(myIdentifier, m, null);
+        fillTableWithEntries(myIdentifier, m);
     }
 
     public FingerTableEntry[] getTable() {
         return table;
     }
 
+    /**
+     * Calculate the start value for the interval associated with an entry in the finger table
+     * @param thisNodeIdentifier the identifier for a node
+     * @param i the index in the finger table
+     * @return the value for the interval
+     */
     public int calculateStartForIndexI(int thisNodeIdentifier, int i) {
         return (int) ((thisNodeIdentifier + Math.pow(2, i)) % Math.pow(2, m));
     }
 
+    /**
+     * Calculate the start and end values for an interval associated with an entry in the finger table
+     * @param thisNodeIdentifier the identifier for a node
+     * @param i the index in the finger table
+     * @return the interval
+     */
     public int[] calculateInterval(int thisNodeIdentifier, int i) {
         int[] interval = new int[2];
         interval[0] = calculateStartForIndexI(thisNodeIdentifier, i);
@@ -31,17 +46,25 @@ public class FingerTable {
         return interval;
     }
 
-    private void fillTableWithEntries(int myIdentifier, int m, Node node) {
+    /**
+     * Fills the finger table with entries
+     * @param myIdentifier this nodes identifier
+     * @param m m-bit identifier
+     */
+    private void fillTableWithEntries(int myIdentifier, int m) {
         for (int i = 0; i < m; i++) {
             int start = calculateStartForIndexI(myIdentifier, i);
             int[] interval = calculateInterval(myIdentifier, i);
 
             // Fill table with entries that does not at the moment have any nodes.
-            table[i] = new FingerTableEntry(start, interval, node);
+            table[i] = new FingerTableEntry(start, interval, null);
         }
     }
 
-    public void displayCurrentTable() {
+    /**
+     * Print the current table to the terminal
+     */
+    public synchronized void displayCurrentTable() {
         System.out.println("IP: " + thisNode.getMyIp() +
                 ", PORT: " + thisNode.getMyPort() +
                 ", identifier: " + thisNode.getMyIdentifier());
